fe6b188fd253d9ae52f904b7baec993f22dac363
max <msveshnikov@thumbtack.net>
2016-09-02 12:22:39 +0600

    first

---
+++ src/main/java/lessons/LessonsConfiguration.java
@@ -0,0 +1,10 @@
+package lessons;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Конфигурационный класс Spring IoC контейнера
+ */
+@Configuration
+public class LessonsConfiguration {
+}
---
+++ src/main/java/lessons/starter/Starter.java
@@ -0,0 +1,20 @@
+package lessons.starter;
+
+import lessons.LessonsConfiguration;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Created by max on 02.09.16.
+ */
+public class Starter {
+    private static final Logger logger = LogManager.getLogger(Starter.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting configuration...");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(LessonsConfiguration.class);
+    }
+}
